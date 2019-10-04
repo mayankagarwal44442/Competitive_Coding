69eac2e9850181cdecd8ba45543ef33d685c4741
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2019-10-04 18:28:35 +0530

    solved two sum

--- Algorithmic_puzzles/Hashing/two_sum.cpp
+++ Algorithmic_puzzles/Hashing/two_sum.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//complexity : o(n)
+
 vector<int> sol(vector<int> &A, int B)
 {
-	unordered_map<int,int> ht;
-	for(int i = 0; i < A.size(); ++i)
-	{
-		if(ht.find[A[i]] == ht.end()) ht.insert({B-A[i], i});
-	
-		else
-		{
-			vector<int> sol;
-			sol.push_back(ht[A[i]]+1)
-			sol.push_back(i+1);
-			return sol;
-		}
-	}
+    unordered_map<int,int> ht;
+    vector<int> sol;
+    for(int i = 0; i < A.size(); ++i)
+    {
+        if(ht.find(A[i]) == ht.end()) ht.insert({B-A[i], i});
+    
+        else
+        {
+            sol.push_back(ht[A[i]]+1);
+            sol.push_back(i+1);
+            return sol;
+        }
+    }
+    return sol;
+
 }
 
 
 int main()
 {
 		vector<int> v = {2, 7, 11, 15};
-		cout << sol(v,9) << endl;
+		vector<int> s = sol(v,9);
+		cout << s[0] <<  " " << s[1] << endl;
 }
